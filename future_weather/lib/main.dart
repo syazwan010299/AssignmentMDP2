a3f6bed0a573e8dc99590e10291a19994f238de3
Caitlin-LSJ <Caitlin-LSJ@users.noreply.github.com>
2019-08-07 17:29:34 +0800

    Updates

--- future_weather/lib/main.dart
+++ future_weather/lib/main.dart
@@ -9,57 +9,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyWeatherPage(),
+      home: MyWeatherApp(),
     );
   }
 }
 
-class MyWeatherPage extends StatefulWidget {
-  @override
-  _MyWeatherPageState createState() => _MyWeatherPageState();
-}
-
-class _MyWeatherPageState extends State<MyWeatherPage> {
-  final _pageController = PageController();
-  final _currentPageNotifer = ValueNotifier<int>(0);
-
+class MyWeatherApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: null,
-    );
+    return Placeholder();
   }
+
 }
-
-// class PageBuilds extends StatelessWidget {
-//   final PageController controller = new PageController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: new Container(
-//         padding: EdgeInsets.only(top: 16.0),
-//         decoration: new BoxDecoration(color: Colors.black),
-//         child: Stack(
-//           alignment: FractionalOffset.topCenter,
-//           children: <Widget>[
-//             new PageView(
-//               children: <Widget>[
-//                 Center(child: Text("Kuala Lumpur")),
-//                 Center(child: Text("Selangor")),
-//                 Center(child: Text("Paris"))
-//               ],
-//               controller: controller,
-//             ),
-//             new Container(
-//               margin: EdgeInsets.only(top:16.0, bottom:16.0),
-//             ),
-//             CircularIndicator(
-//               controller, 3, 6.0, Colors.black, Colors.white)
-//             ),
-//           ],
-//         ),
-//       );
-    
-//   }
-// }
