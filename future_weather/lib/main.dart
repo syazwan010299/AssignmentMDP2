b71227497521f1aaa09d3d9ae12aff3fec70970a
IzzatulNurAtiqah <44635160+fikriyah@users.noreply.github.com>
2019-08-09 14:19:10 +0800

    IDs and Names

--- future_weather/lib/main.dart
+++ future_weather/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'about_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,8 +12,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return Placeholder();
 
+=======
+    return Scaffold(
+      appBar : AppBar(title: Text("Kuala Lumpur"),
+      leading: IconButton(
+        icon:Icon(Icons.info),
+        onPressed :(){
+          Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyAboutPage()),
+                );
+        }
+      
+      
+      ),
+      
+      ),
+      
+    );
+    
+>>>>>>> Stashed changes
   }
   
 }
