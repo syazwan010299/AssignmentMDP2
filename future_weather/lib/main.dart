4a5f5d7629b4d937556faef62bc8f6d305b4963e
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2019-08-09 14:35:49 +0800

    huhu

--- future_weather/lib/main.dart
+++ future_weather/lib/main.dart
@@ -28,7 +28,56 @@ void main() => runApp(
 class MyFirstPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    Widget current;
+    Widget forecast;
 
+    var apixu = Provider.of<MyJSONWeather>(context);
+    var apixuNoListen = Provider.of<MyJSONWeather>(context, listen: false);
+
+    if (apixu.forecast == null){
+      current = Text("No Place identified");
+    }else{
+      current = Column(
+        children: <Widget>[
+          Text('Location : ' + apixu.forecast.locationName),
+          Text('Current Last Updated : ' + apixu.forecast.currentLastUpdated),
+          Text('Current Temperature : ${apixu.forecast.currentTemp.toStringAsFixed(1)} c'),
+          Text('Current Condition : ' + apixu.forecast.currentCondition),
+          Text('Wind Speed : ${apixu.forecast.windSpeed.toStringAsFixed(1)}'),
+          Text('Pressure : ${apixu.forecast.pressure.toStringAsFixed(1)}'),
+          Text('Humidity : ${apixu.forecast.humidity.toStringAsFixed(1)}'),
+          Text('Visibility : ${apixu.forecast.visibility.toStringAsFixed(1)}'),
+          Text('UV Index : ${apixu.forecast.uvIndex.toStringAsFixed(1)}'),
+          Text('Sunset : ' + apixu.forecast.sunset),
+          Text('Sunrise : ' + apixu.forecast.sunrise),
+          Text('Moonset : ' + apixu.forecast.moonSet),
+          Text('Moonrise : ' + apixu.forecast.moonRise),
+        ],
+      );
+    }
+
+     return Scaffold(
+      appBar: AppBar(
+        title: Text("Weather"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            current,
+            RaisedButton(
+              child: Text('Refresh'),
+              onPressed: () {
+                apixuNoListen.fetch();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+    /*
     return Scaffold
     (
       appBar:AppBar
@@ -68,4 +117,5 @@ class MyFirstPage extends StatelessWidget{
   }
 
 }
+*/
 
