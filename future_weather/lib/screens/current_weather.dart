748bba7328ba3277eecd18c162f56908b5d4c232
Caitlin-LSJ <Caitlin-LSJ@users.noreply.github.com>
2019-08-11 00:51:26 +0800

    UPDATED LAYOUT PLEASE CHECK

--- future_weather/lib/screens/current_weather.dart
+++ future_weather/lib/screens/current_weather.dart
@@ -4,271 +4,198 @@ import 'package:flutter/material.dart';
 class MyCurrentWeather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.vertical,
-      children: <Widget>[
-        
-            //background images, will comeback for fine-tuning
-            // Container(
-            //   decoration: BoxDecoration(
-            //     image: DecorationImage(
-            //       image: AssetImage(null),
-            //       fit: BoxFit.fill,
-            //     ),
-            //   ),
-            // ),
-
-            //Current temp container
-            Expanded(
-              flex: 3,
-              child: Container(
-                alignment: Alignment.center,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      //TEMPERATURE AND WEATHER CONDITION
-                      padding: EdgeInsets.all(5.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            //JSON ADDED HERE
-                            '29°',
-                            style: TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          Text(
-                            //JSON ADDED HERE
-                            'Mostly Cloudy',
-                            style: TextStyle(fontSize: 8),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            //sunrise/sunset/moonrise/moonset
-            Expanded(
-              flex: 2,
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    //first row: Sunset Sunrise
-                    padding: EdgeInsets.all(5.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('Sunrise: '),
-                            Text('07:30 AM'), //JSON ADD HERE
-                          ],
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('Sunset: '),
-                            Text('07:30 PM'), //JSON ADD HERE
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    //Second row: Moonrise Moonset
-                    padding: EdgeInsets.all(5.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('Moonrise: '),
-                            Text('12:30 PM'), //JSON ADD HERE
-                          ],
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('Moonset: '),
-                            Text('03:30 AM'), //JSON ADD HERE
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            //ICONS: HUMIDITY ETC
-            Expanded(
-              flex: 2,
-              child: Row(
-                children: <Widget>[
-                  Row(
-                    //ROW #1
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Container(
-                                  margin: EdgeInsets.all(5.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text("Humidity",
-                                          style: TextStyle(fontSize: 20.0)),
-                                      SizedBox(height: 12.0),
-                                      Text("69",
-                                          style: TextStyle(
-                                              fontSize:
-                                                  25.0)), //call value from json
-                                      SizedBox(height: 10.0),
-                                      Text("%",
-                                          style: TextStyle(fontSize: 18.0)),
-                                    ],
-                                  )),
-                              Container(
-                                  margin: EdgeInsets.all(5.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text("Precipitation",
-                                          style: TextStyle(fontSize: 20.0)),
-                                      SizedBox(height: 12.0),
-                                      Text("5",
-                                          style: TextStyle(
-                                              fontSize:
-                                                  25.0)), //call value from json
-                                      SizedBox(height: 10.0),
-                                      Text("%",
-                                          style: TextStyle(fontSize: 18.0)),
-                                    ],
-                                  )),
-                              Container(
-                                  margin: EdgeInsets.all(5.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text("Wind",
-                                          style: TextStyle(fontSize: 20.0)),
-                                      SizedBox(height: 12.0),
-                                      Text("14",
-                                          style: TextStyle(
-                                              fontSize:
-                                                  25.0)), //call value from json
-                                      SizedBox(height: 10.0),
-                                      Text("km/h",
-                                          style: TextStyle(fontSize: 18.0)),
-                                    ],
-                                  )),
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            Expanded(
-              //ROW #2
-              flex: 2,
-              child: Row(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Container(
-                                  margin: EdgeInsets.all(5.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text("Humidity",
-                                          style: TextStyle(fontSize: 16.0)),
-                                      SizedBox(height: 12.0),
-                                      Text("69",
-                                          style: TextStyle(
-                                              fontSize:
-                                                  25.0)), //call value from json
-                                      SizedBox(height: 10.0),
-                                      Text("%",
-                                          style: TextStyle(fontSize: 18.0)),
-                                    ],
-                                  )),
-                              Container(
-                                  margin: EdgeInsets.all(5.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text("Precipitation",
-                                          style: TextStyle(fontSize: 20.0)),
-                                      SizedBox(height: 12.0),
-                                      Text("5",
-                                          style: TextStyle(
-                                              fontSize:
-                                                  25.0)), //call value from json
-                                      SizedBox(height: 10.0),
-                                      Text("%",
-                                          style: TextStyle(fontSize: 18.0)),
-                                    ],
-                                  )),
-                              Container(
-                                  margin: EdgeInsets.all(5.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text("Wind",
-                                          style: TextStyle(fontSize: 20.0)),
-                                      SizedBox(height: 12.0),
-                                      Text("14",
-                                          style: TextStyle(
-                                              fontSize:
-                                                  25.0)), //call value from json
-                                      SizedBox(height: 10.0),
-                                      Text("km/h",
-                                          style: TextStyle(fontSize: 18.0)),
-                                    ],
-                                  )),
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            //RAISED BUTTONS
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text('REFRESH'),
-                    onPressed: () {}, //CALL AGAIN
-                  ),
-                  RaisedButton(
-                    child: Text('FORECAST'),
-                    onPressed: () {}, //CALL AGAIN
-                  ),
-                ],
-              ),
-            ),
-          ],
-        );
-
+    return Scaffold(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          _buildCurrentTemp(),
+          _buildCurrentTime(),
+          _buildSunTime(),
+          _buildMoonTime(),
+          _buildFirstIconRow(),
+          _buildSecondIconRow()
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blueAccent[700],
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.refresh, color: Colors.white),
+            title: Text('Refresh', style: TextStyle(color: Colors.white)),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart, color: Colors.white),
+            title: Text('Forecast', style: TextStyle(color: Colors.white)),
+          )
+        ],
+      ),
+    );
   }
+}
+
+Widget _buildCurrentTemp() {
+  return Container(
+    child: SizedBox(
+      height: 200.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            //JSON ADDED HERE
+            '29°',
+            style: TextStyle(
+              fontSize: 75,
+            ),
+          ),
+          Text(
+            //JSON ADDED HERE
+            'Mostly Cloudy',
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _buildCurrentTime() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text('2019-08-09 11:57'), //JSON LOCAL TIME
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text('34°'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text('29°'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget _buildSunTime() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Text('Sunrise: 07:30 AM'),
+      ),
+      Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Text('Moonrise: 02:50 PM'),
+      ),
+    ],
+  );
+}
+
+Widget _buildMoonTime() {
+  //OPTIONAL
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Text('Sunset: 07:30 PM'),
+      ),
+      Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Text('Moonset: 01:50 AM'),
+      ),
+    ],
+  );
+}
+
+Widget _buildFirstIconRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Feels like: '),
+            Text('36'),
+          ],
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Humidity: '),
+            Text('69%'),
+          ],
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Visibility: '),
+            Text('8.0km'),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _buildSecondIconRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Wind: '),
+            Text('36km/h'),
+          ],
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('UV: '),
+            Text('8.3'),
+          ],
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Pressure: '),
+            Text('1010.0mb'),
+          ],
+        ),
+      ),
+    ],
+  );
 }
