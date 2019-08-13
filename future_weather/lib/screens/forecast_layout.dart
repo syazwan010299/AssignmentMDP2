2f940324f1131deca014dc05ff9991043ff89f81
Caitlin-LSJ <Caitlin-LSJ@users.noreply.github.com>
2019-08-13 15:49:33 +0800

    Update forecast_layout.dart

--- future_weather/lib/screens/forecast_layout.dart
+++ future_weather/lib/screens/forecast_layout.dart
@@ -8,8 +8,41 @@ class MyForecastWeather extends StatelessWidget{
       appBar: AppBar(
         title: Text('Forecast Weather'),
       ),
-      
+      body: _buildForecastList()
     );
   }
 
+}
+
+Widget _buildForecastList(){
+  Widget column = Expanded(
+    child:Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('DD-MM-YYYY'),
+        Icon(Icons.cloud),
+        Text('32'),
+        Text('26')
+      ],
+    ),
+  );
+
+  return ListView.builder(
+    itemBuilder: (BuildContext context, int index) {
+      return Card(
+        child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  column,
+                  column,
+                  column,
+                  column,
+                ],
+              ),
+            ),
+      );
+    },
+
+  );
 }
