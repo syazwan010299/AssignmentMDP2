d49680fe300fa557837b2ea859439c4faf6938e8
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2019-08-13 14:16:54 +0800

    lol

--- future_weather/lib/screen/JSONWeather.dart
+++ future_weather/lib/screen/JSONWeather.dart
@@ -6,32 +6,35 @@ class APIXUForecast{
   final String locationName;
   final String currentLastUpdated;
   final double currentTemp;
-  final String currentCondition;
+  final String currentConditionIcon;
+  final String currentConditionText;
   final double windSpeed;
   final double pressure;
   final double humidity;
   final double visibility;
   final double uvIndex;
 
-  final String sunset;
-  final String sunrise;
-  final String moonRise;
-  final String moonSet;
+  // final String sunset;
+  // final String sunrise;
+  // final String moonRise;
+  // final String moonSet;
 
   APIXUForecast(
     {@required this.locationName,
     @required this.currentLastUpdated,
     @required this.currentTemp,
-    @required this.currentCondition,
+    @required this.currentConditionIcon,
+    @required this.currentConditionText,
     @required this.windSpeed,
     @required this.pressure,
     @required this.humidity,
     @required this.visibility,
     @required this.uvIndex,
-    @required this.sunset,
-    @required this.sunrise,
-    @required this.moonRise,
-    @required this.moonSet}
+    // @required this.sunset,
+    // @required this.sunrise,
+    // @required this.moonRise,
+    // @required this.moonSet
+    }
   );
 
   factory APIXUForecast.fromJson(Map<String, dynamic> json){
@@ -40,21 +43,22 @@ class APIXUForecast{
       var condition = currents['condition'];//fetch the value condition under current
       return APIXUForecast(
         locationName: json['location']['name'],
-        currentLastUpdated: currents['last_updated'],
+        currentLastUpdated:currents['last_updated'],
         currentTemp: currents['temp_c'],
-        currentCondition: condition['text']['icon'],//fetch the text and icon under condition
-        windSpeed: json['wind_kph'],
-        pressure: json['pressure_mb'],
-        humidity: json['humidity'],
-        visibility: json['vis_km'],
-        uvIndex: json['uv'],
-        sunset: json['astro']['sunset'],
-        sunrise: json['astro']['sunrise'],
-        moonRise: json['astro']['moonrise'],
-        moonSet: json['astro']['moonset'],
+        currentConditionIcon: currents['condition']['icon'],
+        currentConditionText: currents['condition']['text'],//fetch the text and icon under condition
+        windSpeed:currents['wind_kph'],
+        pressure: currents['pressure_mb'],
+        humidity: currents['humidity'],
+        visibility: currents['vis_km'],
+        uvIndex: currents['uv'],
+       // sunset: json['astro']['sunset'],
+        //sunrise: json['astro']['sunrise'],
+        //moonRise: json['astro']['moonrise'],
+        //moonSet: json['astro']['moonset'],
       );
     }
-    catch(g){
+    catch(e){
       return null;//return null if JSON String is invalid
     }
   }//end of the factory
@@ -65,7 +69,7 @@ class APIXUForecast{
 
 class MyJSONWeather extends ChangeNotifier{
   //the API Key for the JSON //use it wisely
-  static const APIKey = "6734e8dacaf5403eb24104347192407";
+  static const APIKey = "404d704a8c634a97a0b105010192407";
   APIXUForecast _forecast;
   get forecast => _forecast;
 
@@ -80,13 +84,10 @@ class MyJSONWeather extends ChangeNotifier{
 
   fetch() async{
     http.Response response = await http.Client().get(
-      'http://api.apixu.com/v1/forecast.json?key=$APIKey&q=Kuala+Lumpur');//get the Json value  
+      'http://api.apixu.com/v1/forecast.json?key=$APIKey&q=Selangor&days=1');//get the Json value  
       //print the response body
       _forecast = parseJsonForecast(response.body);
       notifyListeners();
 
   }
-
-
-
 }
