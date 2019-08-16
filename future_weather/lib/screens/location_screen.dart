e49d4d4357b638d5ea0d945e0dfcb95dcffb0e06
Caitlin-LSJ <Caitlin-LSJ@users.noreply.github.com>
2019-08-16 12:37:54 +0800

    minor changes

--- future_weather/lib/screens/location_screen.dart
+++ future_weather/lib/screens/location_screen.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'JSONWeather.dart';
 import 'about_screen.dart';
-import 'package:http/http.dart' as http;
 
 class MyLocationWeather extends StatefulWidget {
   @override
